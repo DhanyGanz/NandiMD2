6ec59318d15ab78a7dc33d96eb54f2f103a98c16
DhanyGanz <105541316+DhanyGanz@users.noreply.github.com>
2022-05-15 21:17:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283897021821','628113048247','6288292024190']
-global.premium = ['6283897021821']
-global.packname = 'Nandi Bot-MD'
+global.owner = ['6285733374626','628113048247','6288292024190']
+global.premium = ['6285733374626']
+global.packname = 'Roxxy-BotMd'
 global.author = 'Whatsap Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
